23679ee273fd208ceaa952cff491080712917012
Vraj Patel <vp3491094@gmail.com>
2026-01-02 18:40:16 +0530

    minor fixes and ui polish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,16 @@ import Product from './pages/Product'
 import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       <Navbar/>
+      <SearchBar/>
       
       <Routes>
         <Route path='/' element={<Home/>} />
@@ -26,7 +30,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/orders' element={<Orders/>} />
-        <Route path='/product/:productID' element={<Product/>} />
+        <Route path='/product/:productId' element={<Product/>} />
         <Route path='/place-order' element={<PlaceOrder/>} />
         
 
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -73,6 +73,8 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
+import { useEffect } from 'react'
+import Product from '../pages/Product'
 
 export const assets = {
     logo,
