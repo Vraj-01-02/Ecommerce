7b9c5c484df27bbae73d8a95c4bf880de5e1c455
Vraj Patel <vp3491094@gmail.com>
2026-01-09 23:18:38 +0530

    setup admin frontend base

---
+++ admin/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
+import Login from './components/Login'
+import { ToastContainer } from "react-toastify";
+
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const currency ='$'
+
+
+
+  const App = () => {
+  const [token, setToken] = useState(
+  localStorage.getItem("adminToken")
+);
+  
+  
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer/>
+      {token === '' ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="flex-1 mx-8 my-8 text-gray-700 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token}/>} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/order" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
