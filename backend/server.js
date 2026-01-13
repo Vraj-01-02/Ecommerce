fd7fef76cbedea420f4f7ff2cc372eb7cc4c61ce
Vraj Patel <vp3491094@gmail.com>
2026-01-13 23:55:58 +0530

    modify server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import express from 'express'
 import cors from 'cors'
-import dotenv from "dotenv";
+
+
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
-dotenv.config();
-
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 
 //App config
@@ -21,7 +26,9 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter);
-app.use('/api/product', productRouter)
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 app.get('/', (req, res) => {
     res.send("API working")
