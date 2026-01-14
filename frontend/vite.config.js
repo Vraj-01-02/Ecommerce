ed3abc97324566abcec1ae4cb69e0a9de850d409
Vraj Patel <vp3491094@gmail.com>
2026-01-14 23:49:32 +0530

    update dependencies and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+
 import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
     plugins: [
+
         tailwindcss(),
+
     ],
+    server: { port: 5173 }
 })
