8e343b764e006d59157e3ae827094bf40b173892
Vraj Patel <vp3491094@gmail.com>
2026-01-16 23:37:13 +0530

    update server config for admin APIs

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import notificationRoutes from "./routes/notificationRoutes.js";
+
 
 
 //App config
@@ -29,6 +31,8 @@ app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
+app.use("/api/notification", notificationRoutes);
+
 
 app.get('/', (req, res) => {
     res.send("API working")
