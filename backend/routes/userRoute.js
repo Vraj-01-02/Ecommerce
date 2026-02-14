1a79f63df3e57506edcce22fac9a5ff05498ac87
Vraj Patel <vp3491094@gmail.com>
2026-02-14 23:53:16 +0530

    update users routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { loginUser, registerUser, adminLogin, getUserProfile, updateUserProfile, changeUserPassword, forgotPassword, resetPassword } from '../controllers/userController.js'
+import { loginUser, registerUser, getUserProfile, updateUserProfile, changeUserPassword, forgotPassword, resetPassword } from '../controllers/userController.js'
 import authUser from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
-userRouter.post('/admin', adminLogin)
+// Legacy admin route removed - use /api/admin/login instead
 userRouter.post('/profile', authUser, getUserProfile)
 userRouter.post('/update-profile', authUser, updateUserProfile)
 userRouter.post('/change-password', authUser, changeUserPassword)
