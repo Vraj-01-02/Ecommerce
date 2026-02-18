ba02f82312c3bb85a1622bcb44b76491da7bbd59
Vraj Patel <vp3491094@gmail.com>
2026-02-18 22:30:14 +0530

    update about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Title from '../components/Title';
-import NewsletterBox from '../components/NewsletterBox';
 import { assets } from '../assets/assets';
 
 const About = () => {
@@ -12,10 +11,10 @@ const About = () => {
       <div className='my-10 flex flex-col md:flex-row gap-16'>
         <img className='w-full md:max-w-112.5' src={assets.about_img} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
-          <p>Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
+          <p>Fabric was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
           <p>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</p>
           <b className='text-gray-800'>Our Mission</b>
-          <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
+          <p>Our mission at Fabric is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
         </div>
       </div>
       <div className='text-xl py-4'>
@@ -35,7 +34,6 @@ const About = () => {
           <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority</p>
         </div>
       </div>
-      <NewsletterBox/>
     </div>
   )
 }
